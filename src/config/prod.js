9368313dbd2734a3f0bbb306b321f71c59f2d560
BABABUSOLA <queenbusola1@gmail.com>
2021-11-27 19:28:15 +0100

    fix: refactor hard coded connection string to Heroku config var

--- src/config/prod.js
+++ src/config/prod.js
@@ -1,2 +1,11 @@
-{
-}
+export const config = {
+    secrets: {
+      jwt: "devsecret",
+    },
+    dbURL: "mongodb+srv://dbUser:dbPassword@cluster0.z8nyy.mongodb.net/twitter-clone?retryWrites=true&w=majority",
+    cloudinary: {
+      cloud_name: process.env.CLOUD_NAME,
+      api_key: process.env.API_KEY,
+      api_secret: process.env.API_SECRET,
+    },
+  };
